d88e0367b564ee6b38214a9d72e9533668446b65
Joe Gathje <j.gathje6@gmail.com>
2018-06-18 17:58:00 -0500

    donzo

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,7 @@ var cornell = Math.floor(Math.random() * 12) + 1;
 var cher = Math.floor(Math.random() * 12) + 1;
 var randomNum = Math.floor(Math.random() * 101) + 19;
 var wins = 0;
+var lose = 0;
 var score = 0;
 console.log("plant ", plant);
 console.log("freddie ", freddie);
@@ -15,8 +16,12 @@ $(document).ready(function () {
 
     $("#randomNum").text("The Computer Number Is: " + randomNum);
     $("#wins").text("Wins: " + wins);
+    $("#losses").text("Losses: " + lose);
     $("#score").text("Your Score: " + score);
     $("#title").text("ROCK STAR ROULETTE!");
+    $("#video").html('<iframe style="display: block; margin: auto;" width="280" height="157.5" src="https://www.youtube.com/embed/GMdvirU-RBk?&autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>');
+
+    instructions();
 
     $("#plant").on("click",
         function () {
@@ -51,11 +56,11 @@ $(document).ready(function () {
 
 function click() {
     if (score == randomNum) {
-        alert("YOU WIN! Click anywhere to restart!");
+        alert("You scored front row tickets! Get ready to rock! Press ok to see if you can do it again!");
 
         $("body").on("click",
             function () {
-                document.getElementById("champion").style.display = "block"
+                document.getElementById("video").style.display = "block"
             }
         )
         wins++;
@@ -72,12 +77,14 @@ function click() {
     }
 
     else if (score > randomNum) {
-        alert("YOU LOSE! Click anywhere to restart!");
+        alert("The show sold out before you got tickets! Click ok to try again!");
         $("body").on("click",
             function () {
-                document.getElementById("fault").style.display = "block"
+                document.getElementById("video").style.display = "block"
             }
         )
+        lose++;
+        $("losses").text("Loses: " + lose);
         plant = Math.floor(Math.random() * 12) + 1;
         freddie = Math.floor(Math.random() * 12) + 1;
         cornell = Math.floor(Math.random() * 12) + 1;
@@ -91,13 +98,18 @@ function click() {
 
 function hide() {
     $("body").on("click",
-            function () {
-                document.getElementById("champion").style.display = "none"
-            }
-        )
-    $("body").on("click",
-    function () {
-        document.getElementById("fault").style.display = "none"
-    }
-)
+        function () {
+            document.getElementById("video").style.display = "hidden"
+        }
+    )
+}
+
+function instructions() {
+    var txt1 = $("<p></p>").text("Each rock star has a random value 1-12 assigned to them.");
+    var txt2 = $("<p></p>").text("Click the rock star and see how many points it adds to your score.");
+    var txt3 = $("<p></p>").text("Keep clicking until your score equals the computer number.");
+    var txt4 = $("<p></p>").text("If you match the computer score, you win!");
+    var txt5 = $("<p></p>").text("Go over the number, you lose!")
+
+    $("#instructions").append(txt1, txt2, txt3, txt4);
 }
